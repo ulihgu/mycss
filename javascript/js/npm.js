175e2b35e3755e7819e77af76acf26adce35f595
ulihgu <ulihgu@msn.com>
2018-08-14 22:09:39 +0800

    2018-8-8

--- javascript/js/npm.js
+++ javascript/js/npm.js
@@ -1,3 +1,312 @@
+/*14:事件文本事件*/
+/*    var input = document.getElementsByTagName('input')[0];
+   /!* input.oninput = function(e){
+        console.log(this.value);
+    }*!/
+    input.onchange = function(e){
+        console.log(this.value);
+    }*/
+
+/*14:请算键盘按下*/
+ /*   document.onkeypress = function (e) {
+        console.log(String.fromCharCode(e.charCode));
+    }*/
+  /*  document.onkeydown = function (e) {
+        console.log(e);
+    }
+    document.onkeyup = function () {
+        console.log('keyup');
+    }*/
+
+/*13:请算鼠标移动还是点击*/
+ /*   var firstTime =0;
+    var lastTime = 0;
+    var key = false;
+    document.onmousedown = function(){
+        firstTime = new Date().getTime();
+    }
+    document.onmouseup =function(){
+        lastTime = new Date().getTime();
+        if(lastTime-firstTime<300){
+            key =true;
+        }
+    }
+    document.onclick = function(){
+        if(key){
+            console.log('click');
+            key = false;
+        }
+    }*/
+
+/*12:鼠标左右*/
+   /* document.onmousedown = function(e){
+        if(e.button == 0){
+            console.log('左键');
+        }else if(e.button ==2){
+            console.log('右键');
+        }else if(e.button == 1) {
+            console.log('中键');
+        }
+    }*/
+
+/*11:移动物体*/
+/*        var div = document.getElementsByClassName('move')[0];
+        var disX,
+            disY;
+        div.onmousedown = function(e){
+            div.innerText="移动";
+            disX = e.pageX -parseInt(div.style.left);
+            disY = e.pageY -parseInt(div.style.top);
+            document.onmousemove = function(e) {
+                div.innerText="移动";
+                var event = e || window.event;
+                div.style.left = e.pageX - disX + "px";
+                div.style.top = e.pageY - disY + "px";
+            }
+            document.onmouseup = function(){
+                div.innerText="按下";
+                console.log("up");
+                document.onmousemove = null;
+            }
+        }*/
+
+/*10:脚本化CSS-兼容事件处理模式*/
+/*function  addEvent(elem,type,handle) {
+    if(elem.addEventListener){
+        elem.addEventListener(type,handle,false);
+    }else if(elem.attachEvent){
+        elem.attachEvent('on'+type,function () {
+            handle.call(elem);
+        })
+    }else {
+        elem['on' + type] = handle;
+    }
+}*/
+
+/*10:脚本化CSS-取元素兼容模式*/
+/*function getStyle(elem,prop){
+    if(window.getComputedStyle){
+        return window.getComputedStyle(elem,null)[prop];
+    }else {
+        return elem.currentStyle[prop];
+    }
+}
+var div = document.getElementsByTagName('div')[0];
+var timer =0;
+ timer = setInterval(function () {
+   div.style.left = parseInt(getStyle(div,'left'))+1+'px';
+   if(parseInt(div.style.left)>500){
+       clearInterval(timer);
+    }
+},10);*/
+/*9:自动向下看文本 */
+/*var start = document.getElementsByTagName('div')[0];
+var timer = 0;
+start.onclick = function () {
+    if(start.innerText=="开始"){
+       // console.log("开始");
+        start.innerText="暂停";
+      timer = setInterval(function(){
+            window.scrollBy(0,10);
+        },100);
+    }else {
+        start.innerText="开始";
+    //    console.log("暂停");
+        clearInterval(timer);
+    }
+
+}*/
+
+/*8:innerWidth视口尺寸向后兼容 */
+/*function getViewportoffset() {
+  if(window.innerWidth){
+          return{
+              w :window.innerWidth,
+              h :window.innerHeight
+          }
+  }else {
+      if(document.compatMode == "BackCompat"){
+          return{
+              w:document.body.clientWidth,
+              h:document.body.clientHeight
+          }
+      }else {
+          return {
+              w :document.documentElement.clientWidth,
+              h :document.documentElement.clientHeight
+          }
+      }
+  }
+}*/
+
+/*8:求滚动条滚动
+ function getScrolloffset() {
+       if(window.pageXOffset){
+           return{
+               x :window.pageXOffset,
+               y :window.pageYOffset
+           }
+       }  else {}
+       return{
+           x :document.body.scrollLeft + document.documentElement.scrollLeft,
+           y :document.body.scrollTop + document.documentElement.scrollTop
+       }
+ }       */
+
+
+/*8:定时器 时间进位
+var minutesNode = document.getElementsByTagName('input')[0];
+var secondsNode = document.getElementsByTagName('input')[1];
+
+var minutes  = 0,seconds = 0;
+
+var time = setInterval(function(){
+              seconds++;
+              if(seconds==60){
+                  seconds = 0;
+                  minutes++;
+              }
+              secondsNode.value = seconds;
+              minutesNode.value = minutes;
+              if(minutes ==3){
+                  clearInterval(time);
+              }
+},1000)*/
+
+/*7:定时器*/
+/*var date = new Date();
+date.setMinutes(16);
+
+setInterval(function () {
+    if(new Date().getTime()-date.getTime()>1000)
+    {
+        Console.log('时间到了。');
+    }
+},1000);*/
+
+/*6:将目标节点targetNode插到指定节点afterNode后面*/
+/*Element.prototype.insertAfter = function (targetNode,afterNode) {
+    var beforNode = afterNode.nextElementSibling;
+    if(beforNode==null){
+        this.appendChild(targetNode);
+    }else {
+        this.insertBefore(targetNode,beforNode);
+    }
+}
+        var div = document.getElementsByTagName('div')[0];
+        var b = document.getElementsByTagName('b')[0];
+        var span = document.getElementsByTagName('span')[0];
+        var i = document.getElementsByTagName('i')[0];
+        var p =document.createElement('p');
+        var a =document.createElement('a');*/
+/*5:将目标节点顺序逆转*/
+/*Element.prototype.invertedChild=function(){
+  var child = this.children;
+  var len = child.length;
+    for(var j=len-2;j>=0;j--)
+    {
+        console.log(j);
+        this.appendChild(child[j]);
+    }
+    return this;
+}
+var div =document.getElementsByTagName('div')[0];*/
+
+
+/*4:扫雷格__多选择器*/
+/*    var ul = document.getElementsByTagName('ul')[0];
+    ul.onmouseover = function (e) {
+        var event = e||window.event;
+        var target = event.target||event.srcElement;
+
+        target.style.backgroundColor = "rgb(0,255,"+ target.getAttribute('img-date')+")";
+        target.setAttribute('img-date',parseInt(target.getAttribute('img-date'))+6);
+}*/
+
+/*3:移动DIV___多选择器*/
+        /*var btn = document.getElementsByTagName('button')[0];
+        var div =document.createElement('div');
+        document.body.appendChild(div);
+        div.style.width = "100px";
+        div.style.height = "100px";
+        div.style.backgroundColor="red";
+        div.style.position = "absolute";
+        div.style.left = "0";
+        div.style.top ="0";
+        div.style.margin ="100px";
+        var speed = 10;
+        btn.onclick = function(){
+            speed ++;
+        }
+        document.onkeydown = function (e) {
+            // console.log(e); //打印按了什么键
+            switch (e.which) {
+                case 38:
+                    div.style.top = parseInt(div.style.top) -speed + "px";
+                    break;
+                case 40:
+                    div.style.top = parseInt(div.style.top) +speed + "px";
+                    break;
+                case 37:
+                    div.style.left = parseInt(div.style.left) -speed + "px";
+                    break;
+                case 39:
+                    div.style.left = parseInt(div.style.left)+ speed +"px";
+                    break;
+            }
+        }
+*/
+/*2:移动DIV___多选择器*/
+            /*var div =document.createElement('div');
+            document.body.appendChild(div);
+            div.style.width = "100px";
+            div.style.height = "100px";
+            div.style.backgroundColor="red";
+            div.style.position = "absolute";
+            div.style.left = "0";
+            div.style.top ="0";
+            var speed = 1;
+            var timer = setInterval(function(){
+                speed += speed/7;
+                div.style.left = parseInt(div.style.left) + speed +"px";
+                div.style.top = parseInt(div.style.top) + speed +"px";
+                if(parseInt(div.style.top) > 300 && parseInt(div.style.left) > 300){
+                    clearInterval(timer);
+                }
+            },50);*/
+
+/*2:选项卡___多选择器*/
+/*var btn =document.getElementsByTagName('button');
+var div=document.getElementsByClassName('content');
+for(var i=0;i<btn.length;i++){
+    (function(n){
+        btn[n].onclick=function () {
+            for(var j=0;j<btn.length;j++){
+                btn[j].className="";
+                div[j].style.display="none";
+            }
+            this.className="active";
+            div[n].style.display="block";
+        }
+    }(i))
+}*/
+
+/*1:点击一次变红和绿*/
+        /*var div =document.getElementsByTagName('div')[0];
+        div.style.width='100px';
+        div.style.height='100px';
+        div.style.backgroundColor='red';
+
+        var count =0;
+        div.onclick=function () {
+            count ++;
+            if(count%2==1){
+                this.style.backgroundColor='green';
+            }else {
+                this.style.backgroundColor='red';
+            }
+        }*/
+
 /*function type(target) {
     var template={
         "[object Array]" : "array",
